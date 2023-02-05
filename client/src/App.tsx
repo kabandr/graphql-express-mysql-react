3e057f2871231065966083e63353b32e6bd57f8f
Rene Kabanda <renerugwiro@yahoo.fr>
2023-02-05 17:50:09 +0000

    feat: added first iteration of Client App.

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import CreateUser from './Components/CreateUser';
+import UsersList from './Components/UsersList';
+
+function App() {
+  return (
+    <>
+    <CreateUser />
+    <UsersList />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Components/UsersList.tsx
@@ -0,0 +1,16 @@
+import { GET_ALL_USERS } from "../Graphql/Queries"
+import { useQuery } from "@apollo/client"
+
+function UsersList() {
+    const {data, error} = useQuery(GET_ALL_USERS)  
+    return (
+        <>
+        <h2>Users List</h2>
+        {data && data.getAllUsers.map((user: any) => {
+            return <div>{user.name} / {user.username}</div>
+        })}
+        </>
+    )
+}
+
+export default UsersList
